686d643c6210998dcd19b4f46def55a69b068956
Átila Camurça <camurca.home@gmail.com>
2019-10-19 12:20:59 -0300

    CSS - Usando flexbox
    Usando gulp-clean-css ao invés de gulp-cssnano.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var del = require('del');
 var useref = require('gulp-useref');
-var cssnano = require('gulp-cssnano');
 var gulpif = require('gulp-if');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify', ['clean'], function() {
     return gulp.src(['slide_config.js', 'js/*.js'], {base: '.'})
@@ -16,9 +16,19 @@ gulp.task('minify', ['clean'], function() {
 gulp.task('useref', function() {
     return gulp.src('index.html')
         .pipe(useref())
-        .pipe(gulpif('*.css', cssnano()))
+        .pipe(gulpif('*.css', cleanCSS()))
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('html', function() {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function () {
+    return gulp.src(['theme/css/*'], { base: '.' })
+        .pipe(gulp.dest('dist'));
+})
 
 gulp.task('images', function(){
     return gulp.src('img/*.+(png|jpg|jpeg|gif|svg)', {base: '.'})
